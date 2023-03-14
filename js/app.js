9d6ea8ee8572af92b317f9d720177314cfecbc07
unknown <sajjad.noowdeh@gmail.com>
2023-03-14 19:13:56 +0330

    first commit

--- js/app.js
+++ js/app.js
@@ -2,5 +2,8 @@ let numbers = [1, 3, 12, 90, 3, 12, 100]
 let itemRepeatObject = numbers.reduce((obj, currentItem) => {
     return {...obj, [currentItem]: (obj[currentItem] || 0) + 1}
 }, {})
+sa
+
+dfasdf
 console.log(itemRepeatObject);
 
