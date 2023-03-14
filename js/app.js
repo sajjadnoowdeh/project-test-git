0eb8121bb4f50fea6a1a13565be6ebd0e009ca20
unknown <sajjad.noowdeh@gmail.com>
2023-03-14 18:46:21 +0330

    fix: add file app

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let numbers = [1, 3, 12, 90, 3, 12, 100]
+let itemRepeatObject = numbers.reduce((obj, currentItem) => {
+    return {...obj, [currentItem]: (obj[currentItem] || 0) + 1}
+}, {})
+console.log(itemRepeatObject);
+
