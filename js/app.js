fe4a3389e1d83951455745be20ffa00ccb672f63
Sina Kamrava <sinakamrava3118@gmail.com>
2023-03-28 21:19:26 +0330

    add reverse function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
-let numbers = [1, 3, 12, 90, 3, 12, 100]
+let numbers = [1, 3, 12, 90, 3, 12, 200];
 let itemRepeatObject = numbers.reduce((obj, currentItem) => {
-    return {...obj, [currentItem]: (obj[currentItem] || 0) + 1}
-}, {})
-sa
+  return { ...obj, [currentItem]: (obj[currentItem] || 0) + 1 };
+}, {});
+sa;
 
-dfasdf
+dfasdf;
 console.log(itemRepeatObject);
 
+getInput();
+
+function getInput() {
+  const userInputs = [];
+  let input;
+  do {
+    input = +prompt("enter a number");
+    console.log(input);
+    if (input !== 0) {
+      userInputs.unshift(input);
+    }
+  } while (input != 0);
+
+  userInputs.forEach((num) => {
+    console.log(num);
+  });
+}
